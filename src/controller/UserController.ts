c68c9daab7fc1467f4153c5af03dc18d7af12c15
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2024-01-25 20:38:02 -0300

    feat: added login

---
+++ src/controller/UserController.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from "../data-source";
+import { User } from "../model/User";
+import bcrypt from "bcrypt";
+
+export class UserController {
+  async createUser(name: string, email: string, password: string) {
+    const userRepository = AppDataSource.getRepository(User);
+    const userExists = await userRepository.existsBy({
+      email,
+    });
+
+    if (userExists) {
+      throw new Error("Email já está cadastrado");
+    }
+
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.password = await bcrypt.hash(password, 10);
+    return await userRepository.save(user);
+  }
+}
