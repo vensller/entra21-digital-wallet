2dc71f8d75a8aba3d10be67d6b43223cfcb4a9d2
Ivens Diego Muller <ivensdiegomuller@gmail.com>
2024-01-30 21:48:58 -0300

    added authentication middleware

---
+++ src/middleware/AuthenticationMiddleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { SessionController } from "../controller/SessionController";
+
+export interface AuthenticatedRequest extends Request {
+  userId: number;
+}
+
+export class AuthenticationMiddleware {
+  async validateAuthentication(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      const token = request.headers.authorization?.split(" ")[1];
+      const sessionController = new SessionController();
+      const jwtPayload = sessionController.verifyToken(token);
+      (request as AuthenticatedRequest).userId = jwtPayload.userId;
+      next();
+    } catch (error) {
+      return response.status(401).json({
+        error: error.message,
+      });
+    }
+  }
+}
